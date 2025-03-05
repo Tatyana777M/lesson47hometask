3194127b66b1b5ea0f886ef322de360587ed9c8c
Tatyana <Tatyanatatyana777M@gmail.com>
2025-03-05 23:13:50 +0100

    Class Task has finished

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,51 @@
 public class Task {
-}
+        private int number;
+        private String description;
+        private String status;
+        private int daysInProcessing;
+
+        public Task(int number, String description, String status, int daysInProcessing) {
+            this.number = number;
+            this.description = description;
+            this.status = status;
+            this.daysInProcessing = daysInProcessing;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public int getDaysInProcessing() {
+            return daysInProcessing;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%d [%s %d]:%s", number, status, daysInProcessing, description);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Task task = (Task) o;
+
+            return number == task.number;
+        }
+
+        @Override
+        public int hashCode() {
+            return number;
+        }
+
+    }
+
