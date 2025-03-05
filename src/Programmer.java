8014b8885a18abe1d3ec706bcc0ece575c9fccf8
Tatyana <Tatyanatatyana777M@gmail.com>
2025-03-05 23:37:30 +0100

    Class Main has made

--- src/Programmer.java
+++ src/Programmer.java
@@ -1,3 +1,39 @@
-public class Programmer {
+import java.util.List;
 
-}
+public class Programmer {
+        private String name;
+        private String city;
+        private List<Task> tasks;
+
+        public Programmer(String name, String city, List<Task> tasks) {
+            this.name = name;
+            this.city = city;
+            this.tasks = tasks;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public List<Task> getTasks() {
+            return tasks;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s %s Tasks: %s", name, city, tasks);
+        }
+    }
+
